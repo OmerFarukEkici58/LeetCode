b8ed65a3ec287c740509230361ff395c8f56f3ef
OmerFarukEkici58 <ekiciomerfaruk.45@gmail.com>
2023-08-31 09:10:51 +0300

    Q13RomanToIntegerEasy

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,9 +1,7 @@
 public class Deneme {
     public static void main(String[] args) {
-        int x = 121;
-        StringBuilder xNormal = new StringBuilder(String.valueOf(x));
-        StringBuilder xReversed = new StringBuilder(String.valueOf(x));
-        System.out.println(xNormal.equals(xReversed.reverse()));//1.ifade = false
-        System.out.println(xNormal.toString().equals(xReversed.reverse().toString()));//2.ifade = true
+       String s = "Ömererrrr";
+       s = s.replaceFirst("r","");
+        System.out.println(s);
     }
 }
